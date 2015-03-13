307f0c9f265fa879f6c8ff7d3f4ee142e5b7efea
kittys <gampolt@live.com>
2015-03-13 16:34:14 +0700

    Change from couchbase to mongodb. Due to cannot do json marshal during insert And cannot understand couchbase query

--- main.go
+++ main.go
@@ -4,22 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
-	"github.com/couchbase/go-couchbase"
 	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type Person struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	Name      string
+	Phone     string
+	Timestamp time.Time
+}
 
 func el(step string, err error) {
 	if err != nil {
 		log.Fatalf("%v: %v", step, err)
+		//panic(err)
 	}
 }
 
 func main() {
 	r := httprouter.New()
 	r.GET("/", Index)
-	r.GET("/:col/:id", Lists)
+	r.GET("/col/:col/:id", Lists)
+	r.GET("/db", TestDb)
 	http.ListenAndServe(":8080", r)
+}
+
+func Test(w http.ResponseWriter, r *http.Response, p httprouter.Params) {
 }
 
 func Index(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -27,18 +41,24 @@ func Index(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 }
 
 func Lists(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(w, "col: %v\nid: %v", p.ByName("col"), p.ByName("id"))
+	fmt.Fprintf(w, "col: %v\nid iss: %v", p.ByName("col"), p.ByName("id"))
 }
 
-func getDb() {
-	c, err := couchbase.Connect("http://localhost:8091/")
+func TestDb(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	session, err := mgo.Dial("127.0.0.1, 198.10.10.71")
 	el("connect", err)
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
 
-	pool, err := c.GetPool("default")
-	el("get pool", err)
+	c := session.DB("test").C("people")
+	err = c.Insert(
+		&Person{Name: "Ball", Phone: "+66 33 123 456", Timestamp: time.Now()},
+		&Person{Name: "Fah", Phone: "+66 33 123 799", Timestamp: time.Now()})
+	el("insert", err)
 
-	bucket, err := pool.GetBucket("default")
-	el("get bucket", err)
+	var result []Person
+	err = c.Find(bson.M{"name": "Fah"}).Sort("-timestamp").All(&result)
+	el("find", err)
 
-	bucket.Set("someKey", 0, []string{"an", "example", "list"})
+	fmt.Fprintf(w, "%v", result)
 }
